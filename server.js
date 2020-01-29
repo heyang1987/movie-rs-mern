ce0d2d8435f30e096efc6f7c8db169815d040a8d
Yangyang He <yyhe@g.clemson.edu>
2020-01-29 00:13:31 -0500

    try

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("client/build"));
+app.use(express.static(__dirname, "client/build"));
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
