40d03b6461f7285307756ab4010b166cc460e471
Yangyang He <yyhe@g.clemson.edu>
2020-01-28 20:57:33 -0500

    try again

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
@@ -24,7 +24,7 @@ const moviesRouter = require("./routes/movies");
 app.use("/movies", moviesRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 app.listen(port, () => {
